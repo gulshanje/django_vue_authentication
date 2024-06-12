17db035f61cdcabe21115a3c6a23e0c2a123f2fb
Gulshan <shrigulshankumar@gmail.com>
2024-06-12 14:21:23 +0300

    Added postgres database setup

--- backend/core_prj/settings.py
+++ backend/core_prj/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'core_prj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST')
     }
 }
 
