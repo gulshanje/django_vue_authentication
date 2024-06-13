38c020be4722ab1d0f7bfce6195291b73e1c7909
Gulshan <shrigulshankumar@gmail.com>
2024-06-14 02:14:56 +0300

    Added userserializer

--- backend/core_app/views.py
+++ backend/core_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework  import exceptions
+from .serializers import UserSerializer
 # Create your views here.
+
 class RegisterAPIView(APIView):
     def post(self, request):
-        return Response(request.data)
+        data = request.data
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Password do not match')
+        
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data)
