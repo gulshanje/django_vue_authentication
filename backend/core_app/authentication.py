05edcfbf3441754e703c258647eb996a1a52c13f
Gulshan <shrigulshankumar@gmail.com>
2024-06-16 02:36:49 +0300

    Added  JWTAuthentication middleware

--- backend/core_app/authentication.py
+++ backend/core_app/authentication.py
@@ -1,6 +1,21 @@
+from core_app.serializers import UserSerializer
 import jwt, datetime
 from datetime import datetime, timezone, timedelta
 from rest_framework import exceptions
+from rest_framework.authentication import get_authorization_header, BaseAuthentication
+from .models import User
+
+# Create middleware
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        auth = get_authorization_header(request).split()
+        if auth and len(auth) == 2:
+            token = auth[1].decode('utf-8')
+            id = decode_access_token(token)
+            user = User.objects.get(pk=id)
+            return (user, None)
+        raise exceptions.AuthenticationFailed('unauthenticated')
+
 def create_access_token(id):
     return jwt.encode({
         'user_id': id,
