3cb029c0398fbe8da144d95a8232f919c4e34ae1
Gulshan <shrigulshankumar@gmail.com>
2024-06-16 02:14:47 +0300

    Added UserAPI view class

--- backend/core_app/views.py
+++ backend/core_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from core_app.authentication import create_access_token, create_refresh_token
+from core_app.authentication import create_access_token, create_refresh_token, decode_access_token
 from core_app.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework  import exceptions
+from rest_framework.authentication import get_authorization_header
 from .serializers import UserSerializer
 # Create your views here.
 
@@ -41,4 +42,15 @@ class LoginAPIView(APIView):
         return response
         # serializer = UserSerializer(user)
         # return Response(serializer.data)
-        
+
+class UserAPIView(APIView):
+    def get(self, request):
+        auth = get_authorization_header(request).split()
+        if auth and len(auth) == 2:
+            token = auth[1].decode('utf-8')
+            id = decode_access_token(token)
+            user = User.objects.get(pk=id)
+            if user:
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+        raise exceptions.AuthenticationFailed('unauthenticated')
