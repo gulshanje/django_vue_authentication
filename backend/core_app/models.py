8c6fff77b642e8b6a4a5f54e5d9ebbc2617b7731
Gulshan <shrigulshankumar@gmail.com>
2024-06-16 03:13:27 +0300

    Added User token model

--- backend/core_app/models.py
+++ backend/core_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
 class User(AbstractUser):
     first_name = models.CharField( max_length=250)
     last_name = models.CharField( max_length=250)
@@ -10,3 +11,9 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+class UserToken(models.Model):
+    user_id = models.IntegerField()
+    token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expired_at = models.DateTimeField()
