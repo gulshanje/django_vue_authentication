4f0fcc87697a698f0d9ed8fc2e79f99cfb23978f
Gulshan <shrigulshankumar@gmail.com>
2024-06-17 01:42:23 +0300

    Added email setting for testing locally

--- backend/core_prj/settings.py
+++ backend/core_prj/settings.py
@@ -141,3 +141,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Allow frontend to get cookie
 CORS_ALLOW_CREDENTIALS = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='127.0.0.1'
+EMAIL_PORT=1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
