0ed6db484031103458600c1e66243b8fe4dfe7d3
Gulshan <shrigulshankumar@gmail.com>
2024-06-16 03:14:16 +0300

    Added Logout api view

--- backend/core_app/views.py
+++ backend/core_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from core_app.authentication import JWTAuthentication, create_access_token, create_refresh_token, decode_access_token, decode_refresh_token
-from core_app.models import User
+from core_app.models import User, UserToken
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework  import exceptions
 from .serializers import UserSerializer
+from datetime import datetime, timezone, timedelta
 # Create your views here.
 
 class RegisterAPIView(APIView):
@@ -31,7 +32,7 @@ class LoginAPIView(APIView):
         
         access_token = create_access_token(user.id)
         refresh_token = create_refresh_token(user.id)
-
+        UserToken.objects.create(user_id= user.id, token = refresh_token, expired_at =  datetime.now(timezone.utc) + timedelta(days=7), )
         response = Response()
         response.set_cookie(key='refresh_token', value=refresh_token, httponly=True)
         response.data = {
@@ -53,5 +54,22 @@ class RefreshTokenAPIView(APIView):
         refresh_token = request.COOKIES.get('refresh_token')
 
         id = decode_refresh_token(refresh_token)
+
+        if not UserToken.objects.filter( user_id = id, token=refresh_token, expired_at__gt=datetime.now(timezone.utc)).exists():
+            raise exceptions.AuthenticationFailed('Unauthenticated')
+        
         access_token = create_access_token(id)
         return Response({'token':refresh_token})
+
+class LogoutAPIView(APIView):
+    # authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_token')
+        UserToken.objects.filter(token=refresh_token).delete()
+        response = Response()
+        response.delete_cookie(key='refresh_token')
+        response.data = {
+            'message': 'Successfully logout'
+        }
+        return response
