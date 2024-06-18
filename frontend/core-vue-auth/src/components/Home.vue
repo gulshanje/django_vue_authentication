835d8b49e62677cad4401c0218519f5e629c29f4
Gulshan <shrigulshankumar@gmail.com>
2024-06-18 14:31:26 +0300

    Added home page to app

---
+++ frontend/core-vue-auth/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Hello Home Page</h1>
+        <h3>{{ message }}</h3>
+    </div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+import axios from 'axios';
+    export default {
+        name:'HomePage',
+        setup(){
+          const message = ref('You are not logged in') 
+          onMounted(async () => {
+            const {data} =  await axios.get('user')
+            message.value = `Hi! ${data.first_name }, ${data.last_name}` 
+          })
+          
+          return ({
+            message
+          })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
