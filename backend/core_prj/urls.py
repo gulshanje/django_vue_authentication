9c4cf688f969e02e3612864d9c834a7b63e2fe40
Gulshan <shrigulshankumar@gmail.com>
2024-06-18 14:29:27 +0300

    Added core bluugo app to project settings

--- backend/core_prj/urls.py
+++ backend/core_prj/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core_app.urls')),
+    path('upload/', include('core_bluugo_csv.urls')),
 ]
