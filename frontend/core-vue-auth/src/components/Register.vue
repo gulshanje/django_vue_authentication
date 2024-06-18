48685c80620bee5de36afa5e9ac2cca6be43d3c9
Gulshan <shrigulshankumar@gmail.com>
2024-06-18 14:31:48 +0300

    Added register component

---
+++ frontend/core-vue-auth/src/components/Register.vue
@@ -0,0 +1,66 @@
+<template>
+    <main class="form-signin w-100 m-auto">
+  <form @submit.prevent="submit">
+    <img class="mb-4" src="@/assets/dhaba.jpeg" alt="" width="150" height="75">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input v-model="data.first_name" class="form-control"   placeholder="First Name">
+      <label>First Name</label>
+    </div>
+    <div class="form-floating">
+      <input v-model="data.last_name"  class="form-control"   placeholder="Last Name">
+      <label  >Last Name</label>
+    </div>
+    <div class="form-floating">
+      <input v-model="data.email"  type="email" class="form-control"   placeholder="Email">
+      <label  >Email address</label>
+    </div>
+
+    <div class="form-floating">
+      <input v-model="data.password"  type="password" class="form-control"   placeholder="Password">
+      <label >Password</label>
+    </div>
+    <div class="form-floating">
+      <input v-model="data.password_confirm"  type="password" class="form-control"   placeholder="Confirm Password">
+      <label >Confirm Password</label>
+    </div>
+
+    <button class="btn btn-primary w-100 py-2" type="submit">Register</button>
+    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+  </form>
+</main>
+</template>
+
+<script>
+import { reactive } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+    export default {
+        name: 'RegisterPage',
+        setup(){
+          const data = reactive ({
+            first_name: '',
+            last_name: '',
+            email: '',
+            password: '',
+            password_confirm: ''
+            
+          })
+          
+          const router = useRouter();
+          const submit = async () => {
+            await axios.post('register', data)
+            await router.push('/login')
+          }
+          return ({
+            data, submit
+          })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
