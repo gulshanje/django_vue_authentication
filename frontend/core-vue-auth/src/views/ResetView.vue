56e1fd4408e40dc0fb51fe6b83d2fd367bd5b9df
Gulshan <shrigulshankumar@gmail.com>
2024-06-24 15:20:23 +0300

    Updated form class

--- frontend/core-vue-auth/src/views/ResetView.vue
+++ frontend/core-vue-auth/src/views/ResetView.vue
@@ -1,4 +1,6 @@
 <template>
+  <main class="form-signin w-100 m-auto">
+
    <form @submit.prevent="submit">
     <!-- <img class="mb-4" src="@/assets/dhaba.jpeg" alt="" width="150" height="75"> -->
     <h1 class="h3 mb-3 fw-normal">Reset Password</h1>
@@ -15,6 +17,7 @@
     <button class="btn btn-primary w-100 py-2" type="submit">Submit</button>
     <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
   </form>
+  </main>
 </template>
 
 <script>
